9eb25ebebc3264e8d66d2e8d1dcda0f0457f264e
David Gichuru <davidgichuruofficial@gmail.com>
2023-05-24 22:22:51 +0300

    Day 1

--- 100DaysOfCode/app.py
+++ 100DaysOfCode/app.py
@@ -1,4 +1,13 @@
-randomString = str("Hello World")
+randomString = str("Hello world string with some random words")
 randomNumber = int(50)
 
-print(randomString, randomNumber)
+print(randomString[3], len(randomString), randomNumber)
+print("Hello" in randomString)
+
+convertedRandomNumber = str(randomNumber)
+print(convertedRandomNumber, type(convertedRandomNumber))
+print(randomString[0:6])
+print(randomString[6:12].upper(), randomString.replace("o", "0").capitalize())
+
+formatString = f"Hello World string with some random words and a number which is {randomNumber}"
+print(formatString)
