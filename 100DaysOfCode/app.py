511e792d9a5050f4c55f146e688044d600c51f54
David Gichuru <davidgichuruofficial@gmail.com>
2023-05-29 22:07:56 +0300

    Day 6: conditions

--- 100DaysOfCode/app.py
+++ 100DaysOfCode/app.py
@@ -1,9 +1,16 @@
 listOne = [34, 15, 88, 2]
 listTwo = [34, -345, -1, 100]
+listThree = []
 
 def find_smallest_int(listOfIntegers):
     listOfIntegers.sort()
-    print(listOfIntegers)
-    return listOfIntegers[0]
 
+    if len(listOfIntegers) == 0:
+        print("List is empty")
+    else:
+        print(listOfIntegers)
+
+
+find_smallest_int(listOne)
 find_smallest_int(listTwo)
+find_smallest_int(listThree)
