d1e88ec1a071e313afe9f0b8869403cf56972dd5
David Gichuru <davidgichuruofficial@gmail.com>
2023-06-05 23:01:56 +0300

    Day 14: 7kyu codewars kata

--- 100DaysOfCode/app.py
+++ 100DaysOfCode/app.py
@@ -1,35 +1,30 @@
-myArray = ["Keep", "Remove", "Keep", "Remove", "Keep"]
+import math
 
+def is_square(num):
 
-def remove_every_other(array):
-
-    if array is None or len(array) == 0:
-        return 0
-    else:
-        every_other = array[::2]
-        return every_other
-
-
-def check(seq, elem):
-    if seq is None and elem is None:
-        return 0
-    elif elem in seq:
-        return True
-    else:
-        return False
-
-def zero_fuel(distance_to_pump, mpg, fuel_left):
-
-    distance_limit = fuel_left * mpg
-
-    if distance_limit < distance_to_pump:
+    if num < 0:
         return False
     else:
-        return True
+        square_root = int(math.sqrt(num))
 
-def to_jaden_case(string):
-    return string.title()
+        if square_root ** 2 == num:
+            return True
+        else:
+            return False
 
 
 
+is_square(77)
 
+def descending_order(num):
+    if num is None or num <= 0:
+        return 0
+    else:
+        digits = list(map(int, str(num)))
+        digits.sort()
+        digits.reverse()
+
+        string_list = list(map(str, digits))
+        joined_string = "".join(string_list)
+
+        return int(joined_string)
