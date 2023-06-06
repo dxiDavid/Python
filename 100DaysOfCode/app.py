a72772ddf369846d57e8190744f7851260beb9a5
David Gichuru <davidgichuruofficial@gmail.com>
2023-06-06 23:04:32 +0300

    Day 15: 7kyu codewars kata

--- 100DaysOfCode/app.py
+++ 100DaysOfCode/app.py
@@ -1,4 +1,5 @@
 import math
+import re
 
 def is_square(num):
 
@@ -28,3 +29,25 @@ def descending_order(num):
         joined_string = "".join(string_list)
 
         return int(joined_string)
+
+
+my_string = 'double  spaced  words'
+def reverse_words(text):
+
+    separator = r"\s+"
+    reversed_words = []
+
+    matches = re.findall(separator, text)
+    if matches:
+        separator = matches[0]
+
+    word_list = re.split(separator, text)
+
+    for word in word_list:
+        reversed_word = word[::-1]
+        reversed_words.append(reversed_word)
+
+    print(separator.join(reversed_words))
+    return separator.join(reversed_words)
+
+reverse_words(my_string)
